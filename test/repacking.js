9629c21c472a2d75c7f72c52183d2beb57eb9ccb
NaughtySora <naughtysora@proton.me>
2026-05-22 20:25:46 +0300

    test for repacking

--- test/repacking.js
+++ test/repacking.js
@@ -1,28 +1,49 @@
 'use strict';
 
-const { once } = require("node:events");
-const { Readable } = require("node:stream");
-const { describe, it } = require("node:test");
 const assert = require("node:assert/strict");
-const { repacking } = require("../lib/repacking.js");
+const { once } = require("node:events");
+const { Readable, Writable, } = require("node:stream");
+const { pipeline } = require("node:stream/promises");
+const { describe, it } = require("node:test");
+const { repacking, Repacking } = require("../lib/repacking.js");
 
 const CONTENT_LENGTH = 5000;
 
 const buffer = Buffer.from('a'.repeat(CONTENT_LENGTH));
 const byteLength = buffer.byteLength;
 
-const readable = new Readable();
-readable.push(buffer);
-readable.push(null);
-
 describe('repacking', async () => {
   await it('simple', async () => {
-    const LENGTH = 32;
+    const readable = new Readable();
+    readable.push(buffer);
+    readable.push(null);
+    const SIZE = 32;
     const parts = [];
-    for await (const buffer of repacking(readable, LENGTH)) {
-      assert.ok(buffer.byteLength <= LENGTH);
+    for await (const buffer of repacking(readable, { size: SIZE })) {
+      assert.ok(buffer.byteLength <= SIZE);
       parts.push(buffer);
     }
-    assert.equal(parts.length, Math.ceil(byteLength / LENGTH));
+    assert.equal(parts.length, Math.ceil(byteLength / SIZE));
+  });
+
+  await it('Repacking', async () => {
+    const readable = new Readable();
+    readable.push(buffer);
+    readable.push(null);
+    const SIZE = 1000;
+    const parts = [];
+    const writable = new Writable({
+      write(chunk, encoding, next) {
+        parts.push(chunk);
+        assert.equal(chunk.byteLength, SIZE);
+        next();
+      }
+    });
+    await pipeline(
+      readable,
+      Repacking.from({ size: SIZE }),
+      writable,
+    );
+    assert.equal(parts.length, Math.ceil(byteLength / SIZE));
   });
 });
